0bc52eb2cd39c94f619c3bc0f5602be114b9517f
ghiraniuk-svg <ghirani.uk@gmail.com>
2026-04-06 14:35:49 +0800

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,28 +1,11 @@
-const CACHE_NAME = 'c25k-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
+// Self-unregistering service worker - clears all old caches
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  self.registration.unregister();
 });
